20b74c5bea4604d1e75a19ab32807c2843bb714d
mani1214 <78633513+mani1214@users.noreply.github.com>
2021-03-10 10:15:17 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Student
+    {
+        int Id=11;
+        string name="vamshi";
+        string college="mlrit";
+        string Branch="ECE";
+        string BloodGroup="o+";
+
+        public static void College()
+        {
+            Student s1 = new Student();
+            Console.WriteLine(s1.Id);
+            Console.WriteLine(s1.name);
+            Console.WriteLine(s1.college);
+            Console.WriteLine(s1.Branch);
+        }
+        public static void Hospital()
+        {
+            Student s1 = new Student();
+            Console.WriteLine(s1.name);
+            Console.WriteLine(s1.BloodGroup);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student.College();
+            Student.Hospital();
+        }
+    }
+}
